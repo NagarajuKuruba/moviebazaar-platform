764dce148cee05edcbe1a6433c2557f4c8f1bd22
NagarajuKuruba <knagaraj1100@gmail.com>
2026-04-22 22:29:26 +0530

    added all the changes

---
+++ services/user-service/src/main/java/com/moviebazaar/user/HomeController.java
@@ -0,0 +1,23 @@
+package com.moviebazaar.user;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/home")
+public class HomeController {
+
+    @Value("${custom.message}")
+    private String customMessage;
+
+    @RequestMapping("/welcome")
+    public String welcome() {
+        log.info("Received request for welcome endpoint. {}", customMessage);
+        return "Welcome to Movie Bazaar User Service!";
+    }
+}
